a49b38d378f69e6dba5ef2e30595c9f3bbc8da54
Hustin <hustinkava@gmail.com>
2021-04-10 00:46:39 -0600

    portfolio update

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -23,6 +23,9 @@ function Portfolio () {
                     {
                     TweenMax.to(".slide-number div", 0.5,{ x: '-1000px'})
                     TweenMax.to(".slide-detail div", 0.5, { x: "-1000px"})
+
+                    TweenMax.to(".slide-detail-tech div", 0.5, { x: "-1000"})                        
+
                     TweenMax.to(".slide-detail-facts div", 0.5, { x: "-1000px"})
                     TweenMax.to(".swiper-slide-active", 0.5, { scale: 0.85})
                     }}
@@ -40,7 +43,10 @@ function Portfolio () {
                     TweenMax.to(".swiper-slide .slide-img", 1, { rotation: 0})                        
                     TweenMax.to(".slide-number div", 0.3, { x: 0})
                     TweenMax.to(".slide-detail div", 0.4, { x: 0})
-                    TweenMax.to(".slide-detail-facts div", 0.5, { x: 0})                        
+
+                    TweenMax.to(".slide-detail-tech div", 0.5, { x: 0})                        
+
+                    TweenMax.to(".slide-detail-facts div", 0.6, { x: 0})                        
                     TweenMax.to(".swiper-slide-active", 0.5, { scale: 1, ease: Power4.easeOut})
                     TweenMax.to(".swiper-slide-active .slide-number", 0, { autoAlpha: 1})
                     TweenMax.to(".swiper-slide-next .slide-number", 0, { autoAlpha: 0})
@@ -52,7 +58,10 @@ function Portfolio () {
                     TweenMax.to(".swiper-slide .slide-img", 1, { rotation: 0})
                     TweenMax.to(".slide-number div", 0.3, { x: 0})
                     TweenMax.to(".slide-detail div", 0.4, { x: 0})
-                    TweenMax.to(".slide-detail-facts div", 0.5, { x: 0})                        
+
+                    TweenMax.to(".slide-detail-tech div", 0.5, { x: 0})                        
+
+                    TweenMax.to(".slide-detail-facts div", 0.6, { x: 0})                        
                     TweenMax.to(".swiper-slide-active", 0.5, { scale: 1, ease: Power4.easeOut})
                     TweenMax.to(".swiper-slide-active .slide-number", 0, { autoAlpha: 1})
                     TweenMax.to(".swiper-slide-next .slide-number", 0, { autoAlpha: 0})
@@ -90,20 +99,23 @@ function Portfolio () {
                                 <Grid item xs={12} sm={7}>
                                     <div className="slide-detail">
                                     <div><p><span>
-                                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                                        {slide.description}
                                     </span></p></div>
+                                    </div>
+
+                                    <div className="slide-detail-tech">
+                                        <div>
+                                            <h3>Technology:</h3>
+                                        </div>
                                     </div>
 
                                     <div className="slide-detail-facts">
                                         <div>
-                                            {/* {Data.map(answer => {
-                                                return (
-                                                    <div>
-                                                    <img src={answer.test}/>
-                                                    </div>
-                                                )
-                                            })} */}
-                                        {slide.test}
+                                        { Array.isArray(slide.icons) && slide.icons.map(url => {
+                                            return (
+                                                <img key={url} src={url}  alt='Icon'/>
+                                            );
+                                        })}
                                         </div>
                                     </div>
 
